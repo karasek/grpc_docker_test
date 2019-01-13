0799e3a985496aba3c0917da9b7faae55dff7779
Lubos Karasek <l.karasek@quadient.com>
2019-01-13 15:50:43 +0100

    add

---
+++ planner/PlannerHostedService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace planner
+{
+    public class PlannerHostedService : IHostedService
+    {
+        readonly Server _server;
+
+        public PlannerHostedService(Server server)
+        {
+            _server = server;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            //throw new Exception("die");
+            System.Console.WriteLine($"Starting at {_server.Ports.First()}");
+            _server.Start();
+            System.Console.WriteLine("Started");
+            return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            System.Console.WriteLine("Stopping");
+            await _server.ShutdownAsync();
+            System.Console.WriteLine("Stopped");
+        }
+    }
+}
