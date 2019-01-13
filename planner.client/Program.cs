088f35e38a33d609116c2258517dde98b033d474
karasek <lubos.karasek@gmail.com>
2019-01-13 16:42:58 +0100

    warning fix

--- planner.client/Program.cs
+++ planner.client/Program.cs
@@ -10,13 +10,13 @@ namespace planner
 
         public static async Task Main(string[] args)
         {
-            var channel = new Channel("127.0.0.1:5000", ChannelCredentials.Insecure);
+            var channel = new Channel("127.0.0.1:6000", ChannelCredentials.Insecure);
             var client = new Planner.PlannerClient(channel);
 
             var status = client.GetStatus(new StatusQuery());
             Console.WriteLine(status);
 
-            channel.ShutdownAsync().Wait();
+            await channel.ShutdownAsync();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey(); 
         }
